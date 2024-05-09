2dedc9ee88f63ca8fcaca65b18af500c7a0e5cd2
eunli <eunli99012@gmail.com>
2024-05-10 02:38:13 +0900

    Feat: guard, strategy 추가 #1

---
+++ apps/auth/src/guards/jwt-auth.guard.ts
@@ -0,0 +1,23 @@
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UserUnauthorizedException } from '../exceptions/auth.exception';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  async canActivate(context: ExecutionContext) {
+    try {
+      await super.canActivate(context);
+      return true;
+    } catch (error) {
+      if (error instanceof UnauthorizedException) {
+        throw new UserUnauthorizedException('Access token expired.');
+      } else {
+        throw error;
+      }
+    }
+  }
+}
---
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
+import TokenPayload from '../interfaces/token-payload.interface';
+import { UsersService } from 'apps/auth/src/users/users.service';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          return request?.cookies?.michiAccessToken;
+        },
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('JWT_SECRET'),
+      passReqToCallback: true,
+    });
+  }
+  async validate(req: Request, payload: TokenPayload) {
+    const user = await this.usersService.findById(payload.userId);
+    req.user = user; // payload(request의 user 프로퍼티와 jwtService.verify()를 통해 검증해준 user 객체를 동일시해야함)
+    return user;
+  }
+}
