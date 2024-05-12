a68e8697be2afa87ab3669424417d3bfaf927694
eunli <eunli99012@gmail.com>
2024-05-12 21:15:50 +0900

    Feat: 아이디, 닉네임, 비밀번호 형식 유효성검사 추가 #8

--- apps/auth/src/users/dto/create-user.dto.ts
+++ apps/auth/src/users/dto/create-user.dto.ts
@@ -1,17 +1,40 @@
-import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 import { Role } from '../../@types/enums/user.enum';
 
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
+  @Matches(/^[a-zA-Z][a-zA-Z0-9_]{3,19}$/, {
+    message:
+      'userId는 영문자로 시작하고, 영문자, 숫자, 밑줄(_)로만 이루어져야 합니다. 길이는 4자 이상 20자 이하여야 합니다.',
+  })
   readonly userId: string;
 
   @IsString()
   @IsNotEmpty()
+  @MinLength(8, { message: 'password는 최소 8자 이상이어야 합니다.' })
+  @Matches(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]+$/, {
+    message:
+      'password는 최소 1개 이상의 영문자, 숫자, 특수문자를 포함해야 합니다.',
+  })
   readonly password: string;
 
   @IsString()
   @IsNotEmpty()
+  @MaxLength(10, { message: 'nickname은 10자를 넘을 수 없습니다.' })
+  @MinLength(2, { message: 'nickname은 2자 이상이어야 합니다.' })
+  @Matches(/^[a-zA-Z0-9가-힣]+$/, {
+    message:
+      'nickname은 영문자, 숫자, 한글만 허용되며 공백은 허용되지 않습니다.',
+  })
   readonly nickname: string;
 
   @IsString()
