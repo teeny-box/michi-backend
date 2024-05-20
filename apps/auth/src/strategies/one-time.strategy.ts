a2cd66e213d0d01e1c2d909eed84558715ce02db
eunli <eunli99012@gmail.com>
2024-05-20 09:30:12 +0900

    Feat: 일회용 토큰 검증 strategy, guard 추가 #20

---
+++ apps/auth/src/strategies/one-time.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
+import TokenPayload from '../interfaces/token-payload.interface';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class OneTimeStrategy extends PassportStrategy(Strategy, 'one-time') {
+  constructor(private readonly usersService: UsersService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_ONETIME_SECRET,
+      passReqToCallback: true,
+    });
+  }
+  async validate(req: Request, payload: TokenPayload) {
+    const user = await this.usersService.findById(payload._id);
+    req.user = user;
+    return user;
+  }
+}
