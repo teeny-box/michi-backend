cbda3e9877c344a6cfd485a7765e70a90e320e78
eunli <eunli99012@gmail.com>
2024-06-24 22:48:49 +0900

    Feat: 게시글 유효성검사 추가

--- src/domain/posts/dto/create-post.dto.ts
+++ src/domain/posts/dto/create-post.dto.ts
@@ -1,11 +1,17 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreatePostDto {
   @IsString()
+  @MaxLength(26, { message: '게시글 제목은 26자를 넘을 수 없습니다.' })
+  @MinLength(1, { message: '게시글 제목은 최소 1자 이상이어야 합니다.' })
   @IsNotEmpty()
   readonly title: string;
 
   @IsString()
+  @MaxLength(224, {
+    message: '게시글 내용은 공백 포함 224자를 넘을 수 없습니다.',
+  })
+  @MinLength(2, { message: '게시글 내용은 최소 1자 이상이어야 합니다.' })
   @IsNotEmpty()
   readonly content: string;
 }
